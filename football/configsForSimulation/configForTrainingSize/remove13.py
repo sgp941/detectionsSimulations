cf442334c795af3ceba49708eca8bc15e437a5bc
Archunan Muhunthan <archunan.muhunthan12@imperial.ac.uk>
2016-06-14 12:14:59 +0100

    clear

--- football/configsForSimulation/configForTrainingSize/remove13.py
+++ football/configsForSimulation/configForTrainingSize/remove13.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import string
 import os
+import re
 
 def saveFile(filename, data):
 	# Saving new tokenised file
@@ -10,6 +11,13 @@ def saveFile(filename, data):
 	fh = open(dirname + "/" + "config.txt", 'w+')
 	fh.write(str(data))
 
+def saveFileArray(filename, arrayData):
+
+	os.remove(filename)
+	print "SAVING FILE"
+	with open(filename, 'w+') as myfile:
+		for item in arrayData:
+  			myfile.write("%s" % item)
 
 def readFileContents(filename):
 	# Opening Base file, and getting the base config
@@ -19,22 +27,50 @@ def readFileContents(filename):
 
 filenames = [
 "train_pos_12.txt", 
-# "train_pos_24.txt", 
-# "train_pos_48.txt"
-# "train_pos_96.txt", 
-# "train_pos_192.txt", 
-# "train_pos_384.txt", 
-# "train_pos_1536.txt", 
-# "train_pos_768.txt", 
+"train_pos_24.txt", 
+"train_pos_48.txt",
+"train_pos_96.txt", 
+"train_pos_192.txt", 
+"train_pos_384.txt", 
+"train_pos_1536.txt", 
+"train_pos_768.txt", 
 ]
 
 
 
 for filename in filenames:
-	with open(filename) as f:
-	    content = f.readlines()
-	 	print content
 
+	size = int(re.findall(r'\d+', filename)[0])
+	# print
+
+	# print [int(s) for s in filename.split() if s.isdigit()]
+
+	# content = []
+	with open(filename) as f:
+		content = []
+		content = f.readlines()
+		content.pop(0)
+		content = [line for line in content if int(line[3:7]) % 13 != 0]
+
+
+	content = [str(size) + " 1\n"] + content
+	content = content[0:size+1]
+	# print content[0:size]
+	print filename
+
+
+	saveFileArray(filename, content)
+	# print content
+	    # saveFileArray(filename, content)
+	    
+
+
+
+
+
+#DO THIS SHIT<
+#ITERATE LINE BY LINE, ADDING OT NEW CONTENT
+#PUSH TO FUCKER TO NEW FILE
 
 
 
